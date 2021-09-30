d852bcdea6f22c9cdf81b399b879394f92a7b8c1
Hapro <Hapro@DESKTOP-RJT286I>
2021-09-30 21:23:00 +0300

    Добавьте файлы проекта.

---
+++ LibPair/Pair.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibPair
+{
+    public class Pair
+    {        
+        public int Value1 { get; set; }
+        public int Value2 { get; set; }
+        public int Value3 { get; set; }
+        public int Value4 { get; set; }
+        public int[] Result { get; set; }               
+        public Pair(int value1, int value2, int value3, int value4)
+        {
+            Value1 = value1;
+            Value2 = value2;
+            Value3 = value3;
+            Value4 = value4;
+        }
+        private bool ProveValuesEvenWithObject()
+        {            
+            if (Value1 % 2 == 0)
+                if (Value2 % 2 == 0)
+                    if (Value3 % 2 == 0)
+                        if (Value4 % 2 == 0) return true;
+            return false;
+        }
+        public int[] PairCalculateWithObject()
+        {            
+            if (ProveValuesEvenWithObject() == true)
+            {
+                Result = new int[2];
+                Result[0] = Value1 * Value3;
+                Result[1] = Value2 * Value4;
+                return Result;
+            }
+            else return null;
+        }
+        public static bool ProveValuesEvenWithoutObject(int value1, int value2, int value3, int value4)
+        {
+            if (value1 % 2 == 0)
+                if (value2 % 2 == 0)
+                    if (value3 % 2 == 0)
+                        if (value4 % 2 == 0) return true;
+            return false;
+        }
+        public static int[] PairCalculateWithoutObject(int value1, int value2, int value3, int value4)
+        {
+            if (ProveValuesEvenWithoutObject(value1, value2, value3, value4) == true)
+            {
+                int[] mas = new int[2];
+                mas[0] = value1 * value3;
+                mas[1] = value2 * value4;
+                return mas;
+            }
+            else return null;
+        }
+        
+    }
+}
---
+++ PW5/MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibPair;
+
+namespace PW5
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Support_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Программа имеет следующие особенности:\n1) Максимальное число для ввода в полях - пятизначное\n2) Первое произведение - умножение первого числа на третье. Второе произведение - умножение второго числа на четвертое.", "Справка", MessageBoxButton.OK, MessageBoxImage.Information);            
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void AboutProgram_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №5. Задание 8. Создать класс Pair (пара четных чисел). Создать необходимые методы и свойства. Определить метод вычисления произведения чисел. Создать перегруженный метод для вычисления произведения 2 пар чисел - (a,b)*(c,d) = (a*c), (b*d)", "О программе", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void FirstValue_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            FirstResult.Clear();
+            SecondResult.Clear();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {            
+            bool ProveValue1 = int.TryParse(FirstValue.Text, out int value1);
+            bool ProveValue2 = int.TryParse(SecondValue.Text, out int value2);
+            bool ProveValue3 = int.TryParse(ThirdValue.Text, out int value3);
+            bool ProveValue4 = int.TryParse(ForthValue.Text, out int value4);
+            bool ProveValuesEven = Pair.ProveValuesEvenWithoutObject(value1, value2, value3, value4);
+            if (ProveValue1 == true && ProveValue2 == true && ProveValue3 == true && ProveValue4 == true && ProveValuesEven == true)
+            {
+                int[] mas = new int[2];
+                mas = Pair.PairCalculateWithoutObject(value1, value2, value3, value4);
+                FirstResult.Text = mas[0].ToString();
+                SecondResult.Text = mas[1].ToString();
+            }
+            else MessageBox.Show("У вас некорректно введены значения для проведения произведения!", "ОШИБКА!", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
