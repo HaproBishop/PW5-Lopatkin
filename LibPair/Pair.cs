61fe838bac38608e8ad5d406e7a6b895092b0308
Hapro <acfowo45@gmail.com>
2021-09-30 21:37:16 +0300

    Добавлены комментарии для программного модуля "Pair"

--- LibPair/Pair.cs
+++ LibPair/Pair.cs
@@ -5,7 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace LibPair
-{
+{/// <summary>
+/// Класс, производящий умножение пары четных чисел
+/// </summary>
     public class Pair
     {        
         public int Value1 { get; set; }
@@ -19,7 +21,9 @@ namespace LibPair
             Value2 = value2;
             Value3 = value3;
             Value4 = value4;
-        }
+        }/// <summary>
+         /// Проверяет на четность числа при созданном экземпляре объекта</summary>
+         /// <returns>true - в случае, если все числа четные, false - в случае, если есть нечетные числа</returns>
         private bool ProveValuesEvenWithObject()
         {            
             if (Value1 % 2 == 0)
@@ -27,7 +31,10 @@ namespace LibPair
                     if (Value3 % 2 == 0)
                         if (Value4 % 2 == 0) return true;
             return false;
-        }
+        }/// <summary>
+        /// Производит умножение 2 пар чисел при созданном экземпляре объекта
+        /// </summary>
+        /// <returns>Result - массив, где Result[0] - первое произведение, а Result[1] - второе произведение; Иначе - null, если числа не являются четными</returns>
         public int[] PairCalculateWithObject()
         {            
             if (ProveValuesEvenWithObject() == true)
@@ -38,7 +45,14 @@ namespace LibPair
                 return Result;
             }
             else return null;
-        }
+        }/// <summary>
+         /// Проверяет на четность числа без создания экземпляра объекта
+         /// </summary>
+         /// <param name="value1">Первое число</param>
+         /// <param name="value2">Второе число</param>
+         /// <param name="value3">Третье число</param>
+         /// <param name="value4">Четвертое число</param>
+         /// <returns>true - в случае, если все числа четные, false - в случае, если есть нечетные числа</returns>
         public static bool ProveValuesEvenWithoutObject(int value1, int value2, int value3, int value4)
         {
             if (value1 % 2 == 0)
@@ -46,7 +60,14 @@ namespace LibPair
                     if (value3 % 2 == 0)
                         if (value4 % 2 == 0) return true;
             return false;
-        }
+        }/// <summary>
+         ///  Производит умножение 2 пар чисел без созданного экземпляра объекта
+         /// </summary>
+         /// <param name="value1">Первое число</param>
+         /// <param name="value2">Второе число</param>
+         /// <param name="value3">Третье число</param>
+         /// <param name="value4">Четвертое число</param>
+         /// <returns>null - в случае, если числа не являются четными, mas - в случае, если числа являются четными</returns>
         public static int[] PairCalculateWithoutObject(int value1, int value2, int value3, int value4)
         {
             if (ProveValuesEvenWithoutObject(value1, value2, value3, value4) == true)
