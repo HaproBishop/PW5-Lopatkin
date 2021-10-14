4adff777e0aa4994617558ea9fe4850b3e612d11
HaproBishop <haprobishop@yandex.ru>
2021-10-14 21:25:40 +0300

    Выполнено огромное упрощение кода - Класс стал меньше - Более не используется static для метода, работает только при создании объекта

--- LibPair/Pair.cs
+++ LibPair/Pair.cs
@@ -9,125 +9,29 @@ namespace LibPair
 /// Класс, производящий умножение пары четных чисел
 /// </summary>
     public class Pair
-    {        
+    {
+        int[] _result;
         public int Value1 { get; set; }
         public int Value2 { get; set; }
-        public int Value3 { get; set; }
-        public int Value4 { get; set; }
-        public int[] Result { get; set; }
-        public Pair() { }
-        public Pair(int value1)
-        {
-            Value1 = value1;
-        }
+        public int[] Result { get {return _result; } }
         public Pair(int value1, int value2)
         {
             Value1 = value1;
             Value2 = value2;
         }
-        public Pair(int value1, int value2, int value3)
+        private bool ProveValues(Pair twopair)
         {
-            Value1 = value1;
-            Value2 = value2;
-            Value3 = value3;
-        }
-
-        public Pair(int value1, int value2, int value3, int value4)
-        {
-            Value1 = value1;
-            Value2 = value2;
-            Value3 = value3;
-            Value4 = value4;
-        }/// <summary>
-         /// Проверяет на четность числа при созданном экземпляре объекта</summary>
-         /// <returns>true - в случае, если все числа четные, false - в случае, если есть нечетные числа</returns>
-        private bool ProveValuesEvenWithObject()
-        {            
-            if (Value1 % 2 == 0)
-                if (Value2 % 2 == 0)
-                    if (Value3 % 2 == 0)
-                        if (Value4 % 2 == 0) return true;
+            if (Value1 % 2 == 0 && Value2 % 2 == 0 && twopair.Value1 % 2 == 0 && twopair.Value2 % 2 == 0) return true;
             return false;
-        }/// <summary>
-        /// Производит умножение 2 пар чисел при созданном экземпляре объекта
-        /// </summary>
-        /// <returns>Result - массив, где Result[0] - первое произведение, а Result[1] - второе произведение; Иначе - null, если числа не являются четными</returns>
-        public int[] PairCalculateWithObject()
-        {
-            return PairCalculateWithObjectAction();
         }
-
-        public int[] PairCalculateWithObject(int value1)
+        public void PairCalculate(Pair twopair)
         {
-            Value1 = value1;
-            return PairCalculateWithObjectAction();
-        }
-        public int[] PairCalculateWithObject(int value1,int value2)
-        {
-            Value1 = value1;
-            Value2 = value2;
-            return PairCalculateWithObjectAction();
-        }
-        public int[] PairCalculateWithObject(int value1, int value2, int value3)
-        {
-            Value1 = value1;
-            Value2 = value2;
-            Value3 = value3;
-            return PairCalculateWithObjectAction();
-        }
-        public int[] PairCalculateWithObject(int value1, int value2, int value3, int value4)
-        {
-            Value1 = value1;
-            Value2 = value2;
-            Value3 = value3;
-            Value4 = value4;
-            return PairCalculateWithObjectAction();
-        }
-
-        private int[] PairCalculateWithObjectAction()
-        {
-            if (ProveValuesEvenWithObject() == true)
+            if (ProveValues(twopair) == true)
             {
-                Result = new int[2];
-                Result[0] = Value1 * Value3;
-                Result[1] = Value2 * Value4;
-                return Result;
-            }
-            else return null;
-        }/// <summary>
-        /// Проверяет на четность числа без создания экземпляра объекта
-        /// </summary>
-        /// <param name="value1">Первое число</param>
-        /// <param name="value2">Второе число</param>
-        /// <param name="value3">Третье число</param>
-        /// <param name="value4">Четвертое число</param>
-        /// <returns>true - в случае, если все числа четные, false - в случае, если есть нечетные числа</returns>
-        public static bool ProveValuesEvenWithoutObject(int value1, int value2, int value3, int value4)
-        {
-            if (value1 % 2 == 0)
-                if (value2 % 2 == 0)
-                    if (value3 % 2 == 0)
-                        if (value4 % 2 == 0) return true;
-            return false;
-        }/// <summary>
-         ///  Производит умножение 2 пар чисел без созданного экземпляра объекта
-         /// </summary>
-         /// <param name="value1">Первое число</param>
-         /// <param name="value2">Второе число</param>
-         /// <param name="value3">Третье число</param>
-         /// <param name="value4">Четвертое число</param>
-         /// <returns>null - в случае, если числа не являются четными, mas - в случае, если числа являются четными</returns>
-        public static int[] PairCalculateWithoutObject(int value1, int value2, int value3, int value4)
-        {
-            if (ProveValuesEvenWithoutObject(value1, value2, value3, value4) == true)
-            {
-                int[] mas = new int[2];
-                mas[0] = value1 * value3;
-                mas[1] = value2 * value4;
-                return mas;
-            }
-            else return null;
+                _result = new int[2];
+                _result[0] = Value1 * twopair.Value1;
+                _result[1] = Value2 * twopair.Value2;
+            }                 
         }
-        
     }
 }
