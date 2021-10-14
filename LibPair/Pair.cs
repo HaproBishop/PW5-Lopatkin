09172a2d6bf93e199427f0de288206e6f419da71
HaproBishop <haprobishop@yandex.ru>
2021-10-14 21:35:42 +0300

    Добавлены: -"in" -комментарии, необходимые для методов

--- LibPair/Pair.cs
+++ LibPair/Pair.cs
@@ -18,13 +18,20 @@ namespace LibPair
         {
             Value1 = value1;
             Value2 = value2;
-        }
-        private bool ProveValues(Pair twopair)
+        }/// <summary>
+        /// Происходит проверка значений на четность
+        /// </summary>
+        /// <param name="twopair">Объект, хранящий в себе 2 числа</param>
+        /// <returns>true - в случае, когда все числа четные и false - в случае, когда не все числа четные</returns>
+        private bool ProveValues(in Pair twopair)
         {
             if (Value1 % 2 == 0 && Value2 % 2 == 0 && twopair.Value1 % 2 == 0 && twopair.Value2 % 2 == 0) return true;
             return false;
-        }
-        public void PairCalculate(Pair twopair)
+        }/// <summary>
+         /// Выполнение умножения пар четных чисел
+         /// </summary>
+         /// <param name="twopair">Объект, хранящий в себе 2 числа</param>
+        public void PairCalculate(in Pair twopair)
         {
             if (ProveValues(twopair) == true)
             {
